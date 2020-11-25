6008bb48f1bc98ff03319021c8fbceee3285931b
Diego <soterg.uhdeia@gmail.com>
2020-11-25 18:10:05 -0500

    06 uniendo fronty back

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CrearComponent } from './catalogo/crear/crear.component';
+import { ListarComponent } from './catalogo/listar/listar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'',
+  component: HomeComponent,
+  pathMatch: 'full'
+},
+{
+  path: 'listarPrendas',
+  component: ListarComponent
+},
+{
+  path: 'crearPrendas',
+  component: CrearComponent
+},
+{
+  path: 'login',
+  component: LoginComponent
+},
+{
+  path: 'registro',
+  component: RegistroComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { CrearComponent } from './catalogo/crear/crear.component';
+import { ListarComponent } from './catalogo/listar/listar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    MenuComponent,
+    HomeComponent,
+    CrearComponent,
+    ListarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
